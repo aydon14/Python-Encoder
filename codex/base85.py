e47423d3c3f89c820c7cd53797e6a52f48349c19
aydon14 <124808730+aydon14@users.noreply.github.com>
2023-11-14 12:44:52 -0500

    Deleted 'codex': Obsolete

--- codex/base85.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def encode(input): 
-    output = base64.b85encode(input.encode('utf-8')).decode('utf-8')
-    return output
-
-def decode(input):
-    output = base64.b85decode(input.encode('utf-8')).decode('utf-8')
-    return output
